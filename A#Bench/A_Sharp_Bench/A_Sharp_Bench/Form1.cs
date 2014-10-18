30a603ad56ad7a72429d04e3f4047f36b8c3e2b4
Alex Marinescu <MarinescuAlex10@gmail.com>
2014-10-18 11:41:56 +0300

    First Commit Second commit

---
+++ A#Bench/A_Sharp_Bench/A_Sharp_Bench/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+/*A#Bench namespace*/ 
+namespace A_Sharp_Bench
+{
+    /**/ 
+    public partial class A_Sharp_Bench : Form
+    {
+        /*Component Initialization*/ 
+        public A_Sharp_Bench()
+        {
+            InitializeComponent();
+        }
+
+
+        /*Launch button*/ 
+        private void Launch_Click(object sender, EventArgs e)
+        {
+
+            /*Just for check*/ 
+
+
+
+        }
+
+
+
+    }
+}
