68558095d004838fd579b5981f91b92370635ae8
Alex Marinescu <MarinescuAlex10@gmail.com>
2014-10-18 11:41:56 +0300

    First Commit

---
+++ A#Bench/A_Sharp_Bench/A_Sharp_Bench/Algorithms.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+/**/ 
+namespace A_Sharp_Bench
+{
+
+    /*Algorithm class*/ 
+    public class Algorithms
+    {
+
+        /*Prime Calculation*/ 
+        public bool Prime_calculation(long Number)
+        {
+            bool Prime_isTrue = true;
+            long i;
+            int Counter = 0; 
+
+            /**/ 
+            for (i = 2; i < (long)(Number / 2); i++)
+            {
+                /**/
+                if ((Number % i) == 0)
+                {
+                    Counter++;
+                }
+                else
+                {
+                  /*Do nothing*/ 
+                }
+            }
+
+            /**/
+            if (Counter != 0) {Prime_isTrue = false;}
+            else {Prime_isTrue = true;}
+
+
+
+            return Prime_isTrue; 
+        }
+
+
+
+
+        /*Mersenne Prime Calculation*/
+        public bool Mersenne_Prime_Calculation(long Number)
+        {
+            bool isTrue = true;
+
+            return isTrue; 
+         }
+
+
+
+
+    }
+
+
+
+}
---
+++ A#Bench/A_Sharp_Bench/A_Sharp_Bench/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace A_Sharp_Bench
+{
+    partial class A_Sharp_Bench
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Launch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Launch
+            // 
+            this.Launch.Location = new System.Drawing.Point(338, 38);
+            this.Launch.Name = "Launch";
+            this.Launch.Size = new System.Drawing.Size(107, 84);
+            this.Launch.TabIndex = 0;
+            this.Launch.Text = "Launch";
+            this.Launch.UseVisualStyleBackColor = true;
+            this.Launch.Click += new System.EventHandler(this.Launch_Click);
+            // 
+            // A_Sharp_Bench
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 415);
+            this.Controls.Add(this.Launch);
+            this.Name = "A_Sharp_Bench";
+            this.Text = "A#Bench";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Launch;
+    }
+}
+
